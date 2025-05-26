ff553071e7b2b6e5a88a5cd24af110cbe101b67c
Valres <104681026+ValresMC@users.noreply.github.com>
2025-05-26 16:19:48 +0200

    Update Overwrite.php

--- src/Overwrite.php
+++ src/Overwrite.php
@@ -2,61 +2,61 @@
 
 declare(strict_types=1);
 
-use pocketmine\block\Block;
+use Closure;
 use pocketmine\block\RuntimeBlockStateRegistry;
 use pocketmine\data\bedrock\item\SavedItemData;
 use pocketmine\inventory\CreativeInventory;
 use pocketmine\item\Item;
 use pocketmine\item\StringToItemParser;
 use pocketmine\world\format\io\GlobalItemDataHandlers;
+use ReflectionException;
+use ReflectionProperty;
+use Throwable;
 
 final class Overwrite
 {
-    protected array $creative;
-
-    protected array $deserializers;
-    protected array $itemSerializers;
-
-    protected array $typeIndex;
-
+    /**
+     * Overwrite an item.
+     *
+     * @param  string $typeName
+     * @param  Item   $item
+     * @return void
+     */
     public static function overwriteItem(string $typeName, Item $item): void {
-        $creativeInventory = CreativeInventory::getInstance();
-        $creativeIndex = $creativeInventory->getItemIndex($item);
+        try {
+            $creativeInventory = CreativeInventory::getInstance();
 
-        $creativeInventory->remove($item);
-        StringToItemParser::getInstance()->override($typeName, fn() => $item);
+            $creativeInventory->remove($item);
+            StringToItemParser::getInstance()->override($typeName, fn() => clone $item);
 
-        (function(string $id, Closure $deserializer): void {
-            $this->deserializers[$id] = $deserializer;
-        })->call(GlobalItemDataHandlers::getDeserializer(), $typeName, fn() => clone $item);
+            $deserializer = GlobalItemDataHandlers::getDeserializer();
+            (function(string $id, Closure $deserializer): void {
+                $this->deserializers[$id] = $deserializer;
+            })->call($deserializer, $typeName, fn() => clone $item);
 
-        (function(Item $item, Closure $serializer): void {
-            $this->itemSerializers[$item->getTypeId()] = $serializer;
-        })->call(GlobalItemDataHandlers::getSerializer(), $item, fn() => new SavedItemData($typeName));
+            $serializer = GlobalItemDataHandlers::getSerializer();
+            (function(Item $item, Closure $serializer): void {
+                $this->itemSerializers[$item->getTypeId()] = $serializer;
+            })->call($serializer, $item, fn() => new SavedItemData($typeName));
 
-        (function(Item $item) use ($creativeIndex): void {
-            array_splice($this->creative, $creativeIndex, 0, [$item]);
-            $this->creative = array_values($this->creative);
-        })->call($creativeInventory, $item);
+            $creativeInventory->add($item);
+        } catch (Throwable) {}
     }
 
+    /**
+     * Overwrite a block.
+     *
+     * @param  Closure $blockClosure
+     * @return void
+     * @throws ReflectionException
+     */
+    public static function overwriteBlock(Closure $blockClosure): void {
+        $block = $blockClosure();
+        $runtimeBlockStateRegistry = RuntimeBlockStateRegistry::getInstance();
 
-    /** @throws ReflectionException */
-    public static function overwriteBlock(Block $block): void {
-        $blockRegistry = RuntimeBlockStateRegistry::getInstance();
-        $creativeInv = CreativeInventory::getInstance();
-
-        $creativeInv->remove($block->asItem());
-
-        try {
-            $blockRegistry->register($block);
-        } catch (InvalidArgumentException) {
-            $property = (new ReflectionProperty($blockRegistry, "typeIndex"));
-            $typeIndex = $property->getValue($blockRegistry);
-            $typeIndex[$block->getTypeId()] = $block;
-            $property->setValue($blockRegistry, $typeIndex);
-        }
-
-        $creativeInv->add($block->asItem());
+        $typeIndexProperty = new ReflectionProperty($runtimeBlockStateRegistry, "typeIndex");
+        $value = $typeIndexProperty->getValue($runtimeBlockStateRegistry);
+        $value[$block->getTypeId()] = $block;
+        $typeIndexProperty->setValue($runtimeBlockStateRegistry, $value);
     }
 }
